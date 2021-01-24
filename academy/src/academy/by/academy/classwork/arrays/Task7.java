32386cb86cc4b20b607e04cffd02f313d00e5415
denis <denis@DESKTOP-D0HQ239>
2021-01-24 17:09:02 +0300

    Array Task7..9

---
+++ academy/src/academy/by/academy/classwork/arrays/Task7.java
@@ -0,0 +1,20 @@
+package academy.by.academy.classwork.arrays;
+
+import java.util.Arrays;
+
+public class Task7 {
+	public static void main(String[] args) {
+		int[] array = new int[20];
+		int result = 1;
+
+		// initialising array
+		array[0] = 1;
+		array[1] = 1;
+		for (int i = 2; i < array.length; i++) {
+			array[i] = array[i - 1] + array[i - 2];
+		}
+
+		// print array
+		System.out.println(Arrays.toString(array));
+	}
+}
