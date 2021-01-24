980fb12be205064f54908fa63a42a9c79e938774
denis <denis@DESKTOP-D0HQ239>
2021-01-24 19:38:36 +0300

    Arrays Task10..14 24/01/2021

---
+++ academy/src/academy/by/academy/classwork/arrays/Task11.java
@@ -0,0 +1,49 @@
+package academy.by.academy.classwork.arrays;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task11 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+		int lengthArray;
+
+		// length array
+		System.out.print("Enter number. Even and > 0 - ");
+		while (true) {
+			lengthArray = scanner.nextInt();
+
+			if (lengthArray > 0 && lengthArray % 2 == 0) {
+				scanner.close();
+				break;
+			} else {
+				System.out.println("Oooops...Enter integer. Even and > 0. Thanks");
+			}
+		}
+
+		// initialising array
+		int[] array = new int[lengthArray];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = random.nextInt(11) - 5;
+		}
+		System.out.println(Arrays.toString(array));
+
+		//
+		int moduleOne = 0;
+		int moduleTwo = 0;
+		for (int i = 0; i < array.length / 2; i++) {
+			moduleOne = moduleOne + Math.abs(array[i]);
+			moduleTwo = moduleTwo + Math.abs(array[array.length / 2 + i]);
+		}
+
+		if (moduleOne < moduleTwo) {
+			System.out.println("Left half (" + moduleOne + ") < right half (" + moduleTwo + ")");
+		} else if (moduleOne > moduleTwo) {
+			System.out.println("Left half (" + moduleOne + ") > right half (" + moduleTwo + ")");
+		} else if (moduleOne == moduleTwo) {
+			System.out.println("Left half (" + moduleOne + ") = right half (" + moduleTwo + ")");
+		}
+	}
+}
---
+++ academy/src/academy/by/academy/classwork/arrays/Task14.java
@@ -0,0 +1,35 @@
+package academy.by.academy.classwork.arrays;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Task14 {
+	public static void main(String[] args) {
+		Random random = new Random();
+		int[][] array = new int[8][5];
+
+		// initialising array
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				array[i][j] = random.nextInt(90) + 10;
+			}
+		}
+
+		// print array
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				System.out.print(array[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+		// print array two
+		System.out.println();
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(Arrays.toString(array[i]));
+		}
+
+		// print third
+		System.out.println(Arrays.toString(array));
+	}
+}
